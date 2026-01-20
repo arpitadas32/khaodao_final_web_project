4df21009fad8b4eedce0922ec76bdc03f32d5d82
Samia Rupa <160999005+samiarupaa@users.noreply.github.com>
2026-01-21 01:25:49 +0600

    6

---
+++ customer/delivery_login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Delivery Man Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+    <h1>Delivery Man Login</h1>
+    <form method="post">
+        <input type="text" placeholder="Phone Number" required><br><br>
+        <input type="password" placeholder="Password" required><br><br>
+        <button type="submit">Login</button>
+    </form>
+</div>
+
+</body>
+</html>
