ac29288d7100a3192b6fa1494c42e1c5fd1da163
arpitadas32 <swarnadas735@gmail.com>
2026-01-20 13:22:17 +0600

    28

---
+++ deliveryman/model/Database.php
@@ -0,0 +1,21 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $db   = "khaodao";
+    public $conn;
+
+    public function __construct() {
+        $this->conn = mysqli_connect(
+            $this->host,
+            $this->user,
+            $this->pass,
+            $this->db
+        );
+
+        if (!$this->conn) {
+            die("Database Connection Failed");
+        }
+    }
+}
