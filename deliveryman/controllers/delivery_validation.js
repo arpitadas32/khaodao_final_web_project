51831c3a9136935f03c917f22fd0f651efa624c6
samiarupaa <160999005+samiarupaa@users.noreply.github.com>
2026-01-21 00:34:10 +0600

    Move my files into customer folder

--- deliveryman/controllers/delivery_validation.js
+++
@@ -1,9 +0,0 @@
-function validateOrder() {
-    let id = document.getElementById("order_id").value;
-
-    if (id === "" || id <= 0) {
-        alert("Please enter a valid Order ID");
-        return false;
-    }
-    return true;
-}
--- deliveryman/controllers/historyValidation.js
+++
@@ -1,4 +0,0 @@
-// For future validation / confirmation
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Delivery History Loaded");
-});
